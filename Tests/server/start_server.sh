caef14c0acebaf3fe5f0353b6a9c21fb7bac77b9
snuzi <sabrinuzi@gmail.com>
2020-01-12 18:34:39 +0100

    Fix tests in travis

--- Tests/server/start_server.sh
+++ Tests/server/start_server.sh
@@ -4,7 +4,7 @@ if [ -z ${TRAVIS_JOB_ID} ]; then
     # not running under travis, stay in foreground until stopped
     node server.js
 else
-    cd tests/server
+    cd Tests/server
 
     npm install
     # running under travis, daemonize
